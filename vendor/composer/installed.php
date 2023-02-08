fbb0c451db9c3a7737cf4970dfb0038c173523f9
afaizulh <ijulhakim14@gmail.com>
2023-02-08 08:39:50 +0700

    Update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'hitungpajak/pph21',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'name' => 'counttax/pph21',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '846034a4b8e0e34c1d103250d1b820888d4e805c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'hitungpajak/pph21' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+        'counttax/pph21' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '846034a4b8e0e34c1d103250d1b820888d4e805c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
