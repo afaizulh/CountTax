13999d9e7347588cdd8e717420ff3e429fe985c8
afaizulh <ijulhakim14@gmail.com>
2023-02-08 09:24:15 +0700

    Update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'counttax/pph21',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '846034a4b8e0e34c1d103250d1b820888d4e805c',
+        'reference' => '7dc81027ff9d6be208158c33e2c8d95751550e5a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'counttax/pph21' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '846034a4b8e0e34c1d103250d1b820888d4e805c',
+            'reference' => '7dc81027ff9d6be208158c33e2c8d95751550e5a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
